de3b087111c250f6d5b01327b014419693961de9
Suphada <614259054@webmail.npru.ac.th>
2020-09-28 10:46:14 +0700

    Add file

---
+++ data.js
@@ -0,0 +1,64 @@
+const restaurants = [
+    {
+     "id" : 1,
+     "name" : "After You Dessert Cafe (อาฟเตอร์ยู) - วัน โอ วัน เดอะเทิร์ดเพลส",
+     "type" : "Partner,ชานมไข่มุก",
+     "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/AWiEnah7cEjWIUmPsYSJ/hero/d47aa6ad78d54eacbea79525bccca8d4_1594618084349753539.jpeg"  
+    },
+    {
+    "id" : 2,
+    "name" :"Kamu Tea (คามุ ที) - วัน-โอ-วัน เดอะเทิร์ดเพลส",
+    "type" : "Partner,ชานมไข่มุก",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYLYNKT3JAMTAE/hero/1b1c6992e34148c5b4f52f1ed9897a7b_1588742104186778094.jpeg"
+    },
+    {
+    "id" : 3,
+    "name" :"Sukishi Korean Charcoal Grill (ซูกิชิ) - วัน โอ วัน เดอะ",
+    "type" : "Coupon, Partner, ปิ้งย่าง/บาร์บีคิว",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY5UTJ2WJ8CFCX/hero/9db9657f491a46e6a6aa44efc4c53d58_1600008255303487617.jpg"
+    },
+    {
+    "id" : 4,
+    "name" :"Krispy Kreme (คริสปี้ครีม) - 101 ทรูดิจิทัลพาร์ค",
+    "type" : "Coupon, Partner, ขนมปัง/เบเกอรี่/เค้ก",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZABTFJWCKKEJ2/hero/4c6ddb56bf8c42ffb7601fcbc2795fdb_1595332807551370941.jpeg"
+    },
+    {
+    "id" : 5,
+    "name" :"สวนผัก โอ้กะจู๋ (Ohkajhu) - ดาดฟ้าลาซาล",
+    "type" : "Coupon, Partner, อาหารสุขภาพ",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CY3EGADFJ2X2AT/hero/742d08a710cc442fb8ff71ab0d427d99_1598506540993436486.jpeg"
+    },
+    {
+    "id" : 6,
+    "name" :"วาฬเกยตื้น (The Stranded Whale) - ซอยวชิรธรรม",
+    "type" : "Coupon, Partner, ฟาสต์ฟู้ด",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZECGEEKJLLZJJ/hero/461e2b25581e455b97ed190408aedf31_1589102095824761036.jpeg"
+    },
+    {
+    "id" : 7,
+    "name" :"Cafe by คุณยาย - อุดมสุข 51",
+    "type" : "Coupon, Partner, ขนมปัง/เบเกอรี่/เค้ก",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZBWCB5CDCD3L2/hero/8f654983147a4cb58d635af3f687bfb0_1583402866524572247.jpeg"
+    },
+    {
+    "id" : 8,
+    "name" :"Sushi Hana (ซูชิ ฮานะ) - ถนนบางนา ตราด",
+    "type" : "Coupon, Partner, ซูชิ",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYVUN4L1MFJWRA/hero/22b6e75c4c0b45828e04464faa732cc5_1592749057650460186.jpeg"
+    },
+    {
+    "id" : 9,
+    "name" :"Dakasi (ดาคาซี่) - วัน โอ วัน เดอะเทิร์ดเพลส",
+    "type" : "Partner, ชานมไข่มุก, Herbal drink, กาแฟ/ชา, Milk Tea",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYNZGN2TCULTG6/hero/830e8635c6204cb8bfdb34b4b8411ef5_1600008254836945649.jpg"
+    },
+    {
+    "id" : 10,
+    "name" :"Yoshinoya (โยชิโนยะ) - วัน โอ วัน เดอะเทิร์ดเพลส",
+    "type" : "Partner, ข้าวหน้า",
+    "imageURL" : "https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYKAJEB3UA2TL6/hero/3432da25f0314a1ea5ceb3e9a5d63287_1598195148125147810.jpg"
+    }
+]
+
+module.exports = restaurants;
